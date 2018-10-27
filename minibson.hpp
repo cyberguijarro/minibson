87dfea79c9202229389c4e78a9a59d702f06abbf
Gareth Morgan <gareth@axum.graphics>
2018-10-26 23:39:53 -0400

    Ensure subtype field filed in correct for binary fields

--- minibson.hpp
+++ minibson.hpp
@@ -141,7 +141,7 @@ namespace minibson {
             };
 
             void serialize(void* const buffer, const size_t count) const {
-		size_t strLen = value.length();
+				size_t strLen = value.length();
                 *reinterpret_cast<unsigned int*>(buffer) = strLen + 1;
                 std::memcpy(reinterpret_cast<char*>(buffer) + sizeof(unsigned int), value.c_str(), strLen);
                 *(reinterpret_cast<char*>(buffer) + sizeof(unsigned int) + strLen) = '\0';
@@ -252,6 +252,7 @@ namespace minibson {
                 unsigned char* byte_buffer = reinterpret_cast<unsigned char*>(buffer);
 
                 *reinterpret_cast<int*>(byte_buffer) = value.length;
+				byte_buffer[4] = 0;//Set subtype to 0 ("new" binary subtype)
                 std::memcpy(byte_buffer + 5, value.data, value.length);
             }
 
