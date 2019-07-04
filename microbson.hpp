2738a6229fdf82944f16e4ae643410df57d3f478
andrejlevkovitch <andrejlevkovitch@gmail.com>
2019-07-04 16:24:32 +0300

    change to c++11 and fix bug in microbson (not right get size of string node)

--- microbson.hpp
+++ microbson.hpp
@@ -2,365 +2,302 @@
 
 #include <cstddef>
 #include <cstring>
+#include <iomanip>
+#include <iterator>
 #include <string>
 #include <utility>
-#include <iterator>
-#include <iomanip>
 
-namespace microbson
-{
-    typedef unsigned char byte;
+namespace microbson {
+typedef unsigned char byte;
 
-    enum node_type {
-        double_node = 0x01,
-        string_node = 0x02,
-        document_node = 0x03,
-        binary_node = 0x05,
-        boolean_node = 0x08,
-        null_node = 0x0A,
-        int32_node = 0x10,
-        int64_node = 0x12,
-        unknown_node = 0xFF
-    };
+enum node_type {
+  double_node   = 0x01,
+  string_node   = 0x02,
+  document_node = 0x03,
+  binary_node   = 0x05,
+  boolean_node  = 0x08,
+  null_node     = 0x0A,
+  int32_node    = 0x10,
+  int64_node    = 0x12,
+  unknown_node  = 0xFF
+};
 
-    class document;
+class document;
 
-    template<typename T> struct type_converter { };
+template <typename T>
+struct type_converter {};
 
-    template<> struct type_converter<double>
-    {
-        enum { node_type_code = double_node };
-    };
+template <>
+struct type_converter<double> {
+  enum { node_type_code = double_node };
+};
 
-    template<> struct type_converter<std::string>
-    {
-        enum { node_type_code = string_node };
-    };
+template <>
+struct type_converter<std::string> {
+  enum { node_type_code = string_node };
+};
 
-    template<> struct type_converter<document>
-    {
-        enum { node_type_code = document_node };
-    };
+template <>
+struct type_converter<document> {
+  enum { node_type_code = document_node };
+};
 
-    template<> struct type_converter<void*>
-    {
-        enum { node_type_code = binary_node };
-    };
+template <>
+struct type_converter<void *> {
+  enum { node_type_code = binary_node };
+};
 
-    template<> struct type_converter<bool>
-    {
-        enum { node_type_code = boolean_node };
-    };
+template <>
+struct type_converter<bool> {
+  enum { node_type_code = boolean_node };
+};
 
-    template<> struct type_converter<int>
-    {
-        enum { node_type_code = int32_node };
-    };
+template <>
+struct type_converter<int> {
+  enum { node_type_code = int32_node };
+};
 
-    template<> struct type_converter<long long>
-    {
-        enum { node_type_code = int64_node };
-    };
+template <>
+struct type_converter<long long> {
+  enum { node_type_code = int64_node };
+};
 
-    struct node
-    {
-        byte* bytes;
+struct node {
+  byte *bytes_;
 
-        node() : bytes(NULL) { }
+  node()
+      : bytes_(NULL) {}
 
-        node(byte* bytes) : bytes(bytes) { }
+  node(byte *bytes)
+      : bytes_(bytes) {}
 
-        node_type get_type() const { return static_cast<node_type>(bytes[0]); }
+  node_type get_type() const { return static_cast<node_type>(bytes_[0]); }
 
-        const char* get_name() const
-        {
-            return reinterpret_cast<const char*>( bytes + 1 );
-        }
+  const char *get_name() const {
+    return reinterpret_cast<const char *>(bytes_ + 1);
+  }
 
-        size_t get_size() const
-        {
-            size_t result = 1U + strlen(get_name()) + 1U;
+  size_t get_size() const {
+    size_t result = 1U + strlen(get_name()) + 1U;
 
-            switch (get_type())
-            {
-                case double_node:
-                    result += sizeof(double);
-                    break;
-                case document_node:
-                    result += *reinterpret_cast<int*>(bytes + result);
-                    break;
-                case string_node:
-                case binary_node:
-                    result += (
-                        sizeof(int) 
-                            + *reinterpret_cast<int*>(bytes + result)
-                            + 1U
-                    );
-                    break;
-                case boolean_node:
-                    result += 1U;
-                case null_node:
-                    break;
-                case int32_node:
-                    result += sizeof(int);
-                    break;
-                case int64_node:
-                    result += sizeof(long long);
-                    break;
-                default:
-                    result = 0U;
-                    break;
-            }
+    switch (get_type()) {
+    case double_node:
+      result += sizeof(double);
+      break;
+    case document_node:
+      result += *reinterpret_cast<int *>(bytes_ + result);
+      break;
+    case string_node:
+      result += (sizeof(int) + *reinterpret_cast<int *>(bytes_ + result));
+      break;
+    case binary_node:
+      result += (sizeof(int) + *reinterpret_cast<int *>(bytes_ + result) + 1U);
+      break;
+    case boolean_node:
+      result += 1U;
+    case null_node:
+      break;
+    case int32_node:
+      result += sizeof(int);
+      break;
+    case int64_node:
+      result += sizeof(long long);
+      break;
+    default:
+      result = 0U;
+      break;
+    }
 
-            return result;
-        }
+    return result;
+  }
 
-        void* get_data() const
-        {
-            return bytes + 1U + strlen(get_name()) + 1U;
-        }
+  void *get_data() const { return bytes_ + 1U + strlen(get_name()) + 1U; }
 
-        bool valid(size_t size) const
-        {
-            return (size >= 2) && (get_size() <= size);
-        }
-    };
+  bool valid(size_t size) const { return (size >= 2) && (get_size() <= size); }
+};
 
-    class document
-    {
-        private:
-            byte* bytes;
-            size_t size;
+class document {
+private:
+  byte * bytes_;
+  size_t size_;
 
-            bool lookup(const char* name, node& result) const
-            {
-                byte* iterator = bytes + sizeof(int);
-                size_t left = size - sizeof(int);
-                bool found = false;
+  bool lookup(const char *name, node &result) const {
+    byte * iterator = bytes_ + sizeof(int);
+    size_t left     = size_ - sizeof(int);
+    bool   found    = false;
 
-                result = node(iterator);
+    result = node(iterator);
 
-                while (result.valid(left))
-                {
-                    if (strcmp(result.get_name(), name) == 0)
-                    {
-                        found = true;
-                        break;
-                    }
-                    else
-                    {
-                        iterator += result.get_size();
-                        left -= result.get_size();
-                        result = node(iterator);
-                    }
-                }
+    while (result.valid(left)) {
+      if (strcmp(result.get_name(), name) == 0) {
+        found = true;
+        break;
+      } else {
+        iterator += result.get_size();
+        left -= result.get_size();
+        result = node(iterator);
+      }
+    }
 
-                return found;
-            }
+    return found;
+  }
 
-            template<typename T, typename W>
-                T get(node _node) const
-                {
-                    return static_cast<T>(
-                        *reinterpret_cast<W*>(_node.get_data())
-                    );
-                }
+  template <typename T, typename W>
+  T get(node _node) const {
+    return static_cast<T>(*reinterpret_cast<W *>(_node.get_data()));
+  }
 
-            std::string get_string(const node& _node) const
-            {
-                return std::string(
-                    reinterpret_cast<const char*>(_node.get_data())
-                        + sizeof(int),
-                    *reinterpret_cast<int*>(_node.get_data()) - 1
-                );
-            }
+  std::string get_string(const node &_node) const {
+    return std::string(reinterpret_cast<const char *>(_node.get_data()) +
+                           sizeof(int),
+                       *reinterpret_cast<int *>(_node.get_data()) - 1);
+  }
 
-            template<typename T, typename W>
-                T get(const std::string& name, T _default) const
-                {
-                    node _node;
+  template <typename T, typename W>
+  T get(const std::string &name, T _default) const {
+    node _node;
 
-                    return lookup(name.c_str(), _node)
-                        ? get<T, W>(_node)
-                        : _default
-                    ;
-                }
+    return lookup(name.c_str(), _node) ? get<T, W>(_node) : _default;
+  }
 
-            void dump(const node& _node, std::ostream& _stream) const
-            {
-                switch(_node.get_type())
-                {
-                    case double_node:
-                        _stream << get<double, double>(_node); 
-                        break;
-                    case string_node:
-                        _stream << '"' << get_string(_node) << '"'; 
-                        break;
-                    case binary_node:
-                        {
-                            byte* bytes = reinterpret_cast<byte*>(
-                                _node.get_data()
-                            );
-                            std::ios::fmtflags flags( _stream.flags() );
+  void dump(const node &_node, std::ostream &_stream) const {
+    switch (_node.get_type()) {
+    case double_node:
+      _stream << get<double, double>(_node);
+      break;
+    case string_node:
+      _stream << '"' << get_string(_node) << '"';
+      break;
+    case binary_node: {
+      byte *             bytes = reinterpret_cast<byte *>(_node.get_data());
+      std::ios::fmtflags flags(_stream.flags());
 
-                            _stream
-                                << std::hex 
-                                << std::setw(2) 
-                                << std::setfill('0')
-                            ;
-                            copy(
-                                    bytes + 5U,
-                                    bytes + 5U + *static_cast<int*>(
-                                        _node.get_data()
-                                    ),
-                                    std::ostream_iterator<int>(_stream)
-                            );
-                            _stream.flags(flags);
-                            break;
-                        }
-                    case boolean_node:
-                        _stream << (get<bool, byte>(_node) ? "true" : "false");
-                        break;
-                    case null_node:
-                        _stream << "(null)"; 
-                        break;
-                    case int32_node:
-                        _stream << get<int, int>(_node);
-                        break;
-                    case int64_node:
-                        _stream << get<long long, long long>(_node);
-                        break;
-                    default:
-                        break;
-                }
-            }
+      _stream << std::hex << std::setw(2) << std::setfill('0');
+      copy(bytes + 5U,
+           bytes + 5U + *static_cast<int *>(_node.get_data()),
+           std::ostream_iterator<int>(_stream));
+      _stream.flags(flags);
+      break;
+    }
+    case boolean_node:
+      _stream << (get<bool, byte>(_node) ? "true" : "false");
+      break;
+    case null_node:
+      _stream << "(null)";
+      break;
+    case int32_node:
+      _stream << get<int, int>(_node);
+      break;
+    case int64_node:
+      _stream << get<long long, long long>(_node);
+      break;
+    default:
+      break;
+    }
+  }
 
-        public:
-            document() : bytes(NULL), size(0U) { }
+public:
+  document()
+      : bytes_(NULL)
+      , size_(0U) {}
 
-            document(void* bytes, size_t count)
-                : bytes(reinterpret_cast<byte*>(bytes)), size(count)
-            {
-            }
+  document(void *bytes, size_t count)
+      : bytes_(reinterpret_cast<byte *>(bytes))
+      , size_(count) {}
 
-            bool valid() const
-            {
-                return (size >= 7U) && (bytes[size -1] == 0);
-            }
+  bool valid() const { return (size_ >= 7U) && (bytes_[size_ - 1] == 0); }
 
-            double get(const std::string& name, double _default) const
-            {
-                return get<double, double>(name, _default);
-            }
+  double get(const std::string &name, double _default) const {
+    return get<double, double>(name, _default);
+  }
 
-            std::string get(
-                const std::string& name,
-                const std::string& _default
-            ) const
-            {
-                node _node;
-                bool found = lookup(name.c_str(), _node);
-                std::string result(_default);
+  std::string get(const std::string &name, const std::string &_default) const {
+    node        _node;
+    bool        found = lookup(name.c_str(), _node);
+    std::string result(_default);
 
-                if (found)
-                    result = get_string(_node);
+    if (found)
+      result = get_string(_node);
 
-                return result;
-            }
+    return result;
+  }
 
-            document get(
-                const std::string& name, 
-                const document& _default
-            ) const
-            {
-                node _node;
-                bool found = lookup(name.c_str(), _node);
-                document result(_default);
+  document get(const std::string &name, const document &_default) const {
+    node     _node;
+    bool     found = lookup(name.c_str(), _node);
+    document result(_default);
 
-                if (found)
-                    result = document(_node.get_data(), _node.get_size());
+    if (found)
+      result = document(_node.get_data(), _node.get_size());
 
-                return result;
-            }
+    return result;
+  }
 
-            std::pair<void*, size_t> get(const std::string& name) const
-            {
-                node _node;
-                bool found = lookup(name.c_str(), _node);
-                std::pair<void*, size_t> result(NULL, 0U);
+  std::pair<void *, size_t> get(const std::string &name) const {
+    node                      _node;
+    bool                      found = lookup(name.c_str(), _node);
+    std::pair<void *, size_t> result(NULL, 0U);
 
-                if (found) {
-                    result.second = *reinterpret_cast<int*>(_node.get_data());
-                    result.first = reinterpret_cast<byte*>(_node.get_data()) + 5U;
-                }
+    if (found) {
+      result.second = *reinterpret_cast<int *>(_node.get_data());
+      result.first  = reinterpret_cast<byte *>(_node.get_data()) + 5U;
+    }
 
-                return result;
-            }
+    return result;
+  }
 
-            bool get(const std::string& name, bool _default) const
-            {
-                return get<bool, byte>(name, _default);
-            }
+  bool get(const std::string &name, bool _default) const {
+    return get<bool, byte>(name, _default);
+  }
 
-            int get(const std::string& name, int _default) const
-            {
-                return get<int, int>(name, _default);
-            }
+  int get(const std::string &name, int _default) const {
+    return get<int, int>(name, _default);
+  }
 
-            long long get(const std::string& name, long long _default) const
-            {
-                return get<long long, long long>(name, _default);
-            }
+  long long get(const std::string &name, long long _default) const {
+    return get<long long, long long>(name, _default);
+  }
 
-            void dump(std::ostream& _stream) const
-            {
-                byte* iterator = bytes + sizeof(int);
-                size_t left = size - sizeof(int);
-                node _node(iterator);
+  void dump(std::ostream &_stream) const {
+    byte * iterator = bytes_ + sizeof(int);
+    size_t left     = size_ - sizeof(int);
+    node   _node(iterator);
 
-                _stream << "{ ";
+    _stream << "{ ";
 
-                while (_node.valid(left))
-                {
-                    _stream << _node.get_name() << " : ";
+    while (_node.valid(left)) {
+      _stream << _node.get_name() << " : ";
 
-                    if (_node.get_type() == document_node)
-                        document(
-                                _node.get_data(), 
-                                *static_cast<int*>(_node.get_data())
-                                ).dump(_stream);
-                    else
-                        dump(_node, _stream);
+      if (_node.get_type() == document_node)
+        document(_node.get_data(), *static_cast<int *>(_node.get_data()))
+            .dump(_stream);
+      else
+        dump(_node, _stream);
 
-                    iterator += _node.get_size();
-                    left -= _node.get_size();
-                    _node = node(iterator);
+      iterator += _node.get_size();
+      left -= _node.get_size();
+      _node = node(iterator);
 
+      if (_node.valid(left))
+        _stream << ", ";
+    }
 
-                    if (_node.valid(left))
-                        _stream << ", ";
-                }
+    _stream << " }";
+  }
 
-                _stream << " }";
-            }
+  bool contains(const std::string &name) const {
+    node _node;
 
-            bool contains(const std::string& name)
-            {
-                node _node;
-                
-                return lookup(name.c_str(), _node);
-            }
+    return lookup(name.c_str(), _node);
+  }
 
-            template<typename T>
-            bool contains(const std::string& name)
-            {
-                node _node;
-                bool found = lookup(name.c_str(), _node);
+  template <typename T>
+  bool contains(const std::string &name) const {
+    node _node;
+    bool found = lookup(name.c_str(), _node);
 
-                return (
-                    found
-                    && (_node.get_type() == type_converter<T>::node_type_code)
-                );
-            }
-    };
-}
+    return (found && (static_cast<int>(_node.get_type()) ==
+                      static_cast<int>(type_converter<T>::node_type_code)));
+  }
+};
+} // namespace microbson
--- test.cpp
+++ test.cpp
@@ -1,105 +1,106 @@
-#include "minibson.hpp"
 #include "microbson.hpp"
+#include "minibson.hpp"
 #include <cassert>
 
 void test_minibson();
 void test_microbson();
 
-int main()
-{
-    test_minibson();
-    test_microbson();
-    return 0;
+int main() {
+  test_minibson();
+  test_microbson();
+  return 0;
 }
 
-void test_minibson()
-{
-    using namespace minibson;
-    using namespace std;
+void test_minibson() {
+  using namespace minibson;
+  using namespace std;
 
-    document d;
+  document d;
 
-    d.set("int32", 1);
-    d.set("int64", 140737488355328LL);
-    d.set("float", 30.20);
-    d.set("string", "text");
-    d.set("binary", binary::buffer(&d, sizeof(d)));
-    d.set("boolean", true);
-    d.set("document", document().set("a", 3).set("b", 4));
-    d.set("null");
-    
-    assert(d.contains<int>("int32"));
-    assert(d.contains<long long int>("int64"));
-    assert(d.contains<double>("float"));
-    assert(d.contains<std::string>("string"));
-    assert(d.contains<binary::buffer>("binary"));
-    assert(d.contains<bool>("boolean"));
-    assert(d.contains<document>("document"));
-    assert(d.contains("null"));
+  d.set("int32", 1);
+  d.set("int64", 140737488355328LL);
+  d.set("float", 30.20);
+  d.set("string", "text");
+  d.set("binary", binary::buffer(&d, sizeof(d)));
+  d.set("boolean", true);
+  d.set("document", document().set("a", 3).set("b", 4));
+  d.set("null");
 
-    assert(d.get("int32", 0) == 1);
-    assert(d.get("int64", 0LL) == 140737488355328LL);
-    assert(d.get("float", 0.0) == 30.20);
-    assert(d.get("string", "") == "text");
-    assert(d.get("binary", binary::buffer(NULL, 0)).length == sizeof(d));
-    assert(d.get("boolean", false) == true);
-    assert(d.get("document", document()).contains("a") && d.get("document", document()).contains("b"));
+  assert(d.contains<int>("int32"));
+  assert(d.contains<long long int>("int64"));
+  assert(d.contains<double>("float"));
+  assert(d.contains<std::string>("string"));
+  assert(d.contains<binary::buffer>("binary"));
+  assert(d.contains<bool>("boolean"));
+  assert(d.contains<document>("document"));
+  assert(d.contains("null"));
 
-    size_t size = d.get_serialized_size();	
-    char* buffer = new char[size];
-    d.serialize(buffer, size);
-    
-    document d1(buffer, size);
-    
-    delete[] buffer;
-    
-    assert(d1.get("int32", 0) == 1);
-    assert(d1.get("int64", 0LL) == 140737488355328LL);
-    assert(d1.get("float", 0.0) == 30.20);
-    assert(d1.get("string", "") == "text");
-    assert(d1.get("binary", binary::buffer(NULL, 0)).length == sizeof(d));
-    assert(d1.get("boolean", false) == true);
-    assert(d1.get("document", document()).contains("a") && d.get("document", document()).contains("b"));
-    assert(d1.contains("null"));
+  assert(d.get("int32", 0) == 1);
+  assert(d.get("int64", 0LL) == 140737488355328LL);
+  assert(d.get("float", 0.0) == 30.20);
+  assert(d.get("string", "") == "text");
+  assert(d.get("binary", binary::buffer{}).data.size() == sizeof(d));
+  assert(d.get("boolean", false) == true);
+  assert(d.get("document", document()).contains("a") &&
+         d.get("document", document()).contains("b"));
+
+  size_t size   = d.get_serialized_size();
+  char * buffer = new char[size];
+  d.serialize(buffer, size);
+
+  document d1(buffer, size);
+
+  delete[] buffer;
+
+  assert(d1.get("int32", 0) == 1);
+  assert(d1.get("int64", 0LL) == 140737488355328LL);
+  assert(d1.get("float", 0.0) == 30.20);
+  assert(d1.get("string", "") == "text");
+  assert(d1.get("binary", binary::buffer{}).data.size() == sizeof(d));
+  assert(d1.get("boolean", false) == true);
+  assert(d1.get("document", document()).contains("a") &&
+         d.get("document", document()).contains("b"));
+  assert(d1.contains("null"));
 }
 
-void test_microbson()
-{
-    using namespace std;
+void test_microbson() {
+  using namespace std;
 
-    minibson::document d;
+  minibson::document d;
 
-    d.set("int32", 1);
-    d.set("int64", 140737488355328LL);
-    d.set("float", 30.20);
-    d.set("string", "text");
-    d.set("binary", minibson::binary::buffer(&d, sizeof(d)));
-    d.set("boolean", true);
-    d.set("document", minibson::document().set("a", 3).set("b", 4));
-    d.set("null");
-    
-    size_t size = d.get_serialized_size();	
-    char* buffer = new char[size];
-    d.serialize(buffer, size);
-    
-    microbson::document m(buffer, size);
-    
-    assert(m.contains<int>("int32"));
-    assert(m.contains<long long int>("int64"));
-    assert(m.contains<double>("float"));
-    assert(m.contains<std::string>("string"));
-    assert(m.contains<void*>("binary"));
-    assert(m.contains<bool>("boolean"));
-    assert(m.contains<microbson::document>("document"));
-    assert(m.contains("null"));
+  d.set("int32", 1);
+  d.set("int64", 140737488355328LL);
+  d.set("float", 30.20);
+  d.set("string", "text");
+  d.set("binary", minibson::binary::buffer(&d, sizeof(d)));
+  d.set("boolean", true);
+  d.set("document", minibson::document().set("a", 3).set("b", 4));
+  d.set("some_other_string", "some_other_text");
+  d.set("null");
 
-    assert(m.get("int32", 0) == 1);
-    assert(m.get("int64", 0LL) == 140737488355328LL);
-    assert(m.get("float", 0.0) == 30.20);
-    assert(m.get("string", string("")) == "text");
-    assert(m.get("binary").second == sizeof(d));
-    assert(m.get("boolean", false) == true);
-    assert(m.get("document", microbson::document()).contains("a") && m.get("document", microbson::document()).contains("b"));
-    
-    delete[] buffer;
+  size_t size   = d.get_serialized_size();
+  char * buffer = new char[size];
+  d.serialize(buffer, size);
+
+  const microbson::document m(buffer, size);
+
+  assert(m.contains<int>("int32"));
+  assert(m.contains<long long int>("int64"));
+  assert(m.contains<double>("float"));
+  assert(m.contains<std::string>("string"));
+  assert(m.contains<void *>("binary"));
+  assert(m.contains<bool>("boolean"));
+  assert(m.contains<microbson::document>("document"));
+  assert(m.contains("null"));
+
+  assert(m.get("int32", 0) == 1);
+  assert(m.get("int64", 0LL) == 140737488355328LL);
+  assert(m.get("float", 0.0) == 30.20);
+  assert(m.get("string", string("")) == "text");
+  assert(m.get("binary").second == sizeof(d));
+  assert(m.get("boolean", false) == true);
+  assert(m.get("document", microbson::document()).contains("a") &&
+         m.get("document", microbson::document()).contains("b"));
+
+  delete[] buffer;
 }
